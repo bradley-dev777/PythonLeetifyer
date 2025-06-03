c3341e342411fb120084305910ca120426f130e2
Bradley Hu <hu.yu@wcs-g.com>
2025-06-03 18:14:59 +0000

    Test 1

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,9 +1,12 @@
 import importlib.util
 
 # Dynamically load the HTMLBuilder module
-spec = importlib.util.spec_from_file_location("HTMLBuilder", "./src/Html_Builder_Bradley_Hu/htmlBuilder.py")
+spec = importlib.util.spec_from_file_location("Leetify", "./src/Python_leetifyer_Bradley_Hu/Leetify.py")
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
-HTMLBuilder = module.HTMLBuilder
 
 def test_1():
+    if module.leetify("HeLlO!")=="|-|[-|_|_()!":
+        return True
+    else:
+        return False
